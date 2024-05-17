5a3009a23a22e12352f5aae04f71377bf458aeb7
7Luke7 <pro.chikvaidze@gmail.com>
2024-05-18 01:17:43 +0400

    some point

--- src/Components/Third_Hero.jsx
+++ src/Components/Third_Hero.jsx
@@ -10,8 +10,8 @@ import { A } from "@solidjs/router"
 export const ThirdHero = () => {
     return <div class="mt-24 w-full flex flex-col items-center shadow-2xl rounded-3xl bg-white p-8">
     <h2 class="font-[font-boldest] font-bold mt-3 text-2xl">როგორ მუშაობს</h2>
-    <div class="mt-12 flex flex-col w-full">
-        <div class="flex justify-center gap-x-12 border-l-4 border-slate-300 pl-8">
+    <div class="mt-12 flex flex-col w-full gap-y-5">
+        <div class="flex justify-center w-[80%] gap-x-12 border-l-4 border-slate-300 pl-8">
             <img src={newPost} alt="Post a job" />
             <div>
                 <h2 class="font-[font-bolder] text-xl font-bold">დამკვეთი განათავსებს შეკვეთას</h2>
@@ -20,24 +20,25 @@ export const ThirdHero = () => {
                     <li>შექმენი პოსტი</li>
                 </ul>
                 <div class="flex mt-8 gap-y-2 flex-col">
-                    <A href="/register" class="font-[thin-font] font-bold bg-dark-green hover:bg-dark-green-hover transition ease-in delay-20 text-white px-8 py-2 rounded-[16px]">შექმენი პოსტი</A>
-                    <A href="/register" class="font-[thin-font] font-bold border border-dark-green transition ease-in delay-20 px-8 py-2 rounded-[16px]">გაიგე მეტი</A>
+                    <A href="/register" class="font-[thin-font] font-bold bg-dark-green hover:bg-dark-green-hover transition ease-in delay-20 text-white text-center py-2 px-4 rounded-[16px]">შექმენი პოსტი</A>
+                    <A href="/register" class="font-[thin-font] font-bold border border-dark-green transition ease-in delay-20 px-4 py-2 text-center rounded-[16px]">გაიგე მეტი</A>
                 </div>
             </div>
         </div>
+        <div class="flex justify-center h-[40px] w-[5px] bg-slate-300"></div>
         <div class="flex justify-center gap-x-12 border-l-4 border-slate-300 pl-8">
-            <img src={findWorker} alt="Find a worker" />
-            <div>
+              <div>
                 <h2 class="font-[font-bolder] text-xl font-bold">იპოვე ხელოსანი</h2>
                 <ul class="font-[font-normal] text-lg text-gray mt-3 list-disc ml-5">
                     <li>შეარჩიე გამოცდილი ხელოსანი</li>
-                    <li>დაათვალიერე შეფასებები და შეფასებები</li>
+                    <li>დაათვალიერე შეფასებები</li>
                 </ul>
                 <div class="flex mt-8 gap-y-2 flex-col">
-                    <A href="/register" class="font-[thin-font] font-bold bg-dark-green hover:bg-dark-green-hover transition ease-in delay-20 text-white px-8 py-2 rounded-[16px]">შექმენი პოსტი</A>
-                    <A href="/register" class="font-[thin-font] font-bold border border-dark-green transition ease-in delay-20 px-8 py-2 rounded-[16px]">გაიგე მეტი</A>
+                    <A href="/register" class="font-[thin-font] font-bold text-center bg-dark-green hover:bg-dark-green-hover transition ease-in delay-20 text-white px-8 py-2 rounded-[16px]">შექმენი პოსტი</A>
+                    <A href="/register" class="font-[thin-font] font-bold text-center border border-dark-green transition ease-in delay-20 px-8 py-2 rounded-[16px]">გაიგე მეტი</A>
                 </div>
             </div>
+            <img src={findWorker} alt="Find a worker" />
         </div>
         <div class="flex justify-center gap-x-12 border-l-4 border-slate-300 pl-8">
             <img src={jobComplete} alt="Job complete" />
@@ -48,13 +49,12 @@ export const ThirdHero = () => {
                     <li>მიღება და შეფასება</li>
                 </ul>
                 <div class="flex mt-8 gap-y-2 flex-col">
-                    <A href="/register" class="font-[thin-font] font-bold bg-dark-green hover:bg-dark-green-hover transition ease-in delay-20 text-white px-8 py-2 rounded-[16px]">შექმენი პოსტი</A>
-                    <A href="/register" class="font-[thin-font] font-bold border border-dark-green transition ease-in delay-20 px-8 py-2 rounded-[16px]">გაიგე მეტი</A>
+                    <A href="/register" class="font-[thin-font] font-bold text-center bg-dark-green hover:bg-dark-green-hover transition ease-in delay-20 text-white px-8 py-2 rounded-[16px]">შექმენი პოსტი</A>
+                    <A href="/register" class="font-[thin-font] font-bold text-center border border-dark-green transition ease-in delay-20 px-8 py-2 rounded-[16px]">გაიგე მეტი</A>
                 </div>
             </div>
         </div>
         <div class="flex justify-center gap-x-12 border-l-4 border-slate-300 pl-8">
-            <img src={job_hunt} alt="Job complete" />
             <div>
                 <h2 class="font-[font-bolder] text-xl font-bold">დასრულება</h2>
                 <ul class="font-[font-normal] text-lg text-gray mt-3 list-disc ml-5">
@@ -62,10 +62,11 @@ export const ThirdHero = () => {
                     <li>მიღება და შეფასება</li>
                 </ul>
                 <div class="flex mt-8 gap-y-2 flex-col">
-                    <A href="/register" class="font-[thin-font] font-bold bg-dark-green hover:bg-dark-green-hover transition ease-in delay-20 text-white px-8 py-2 rounded-[16px]">შექმენი პოსტი</A>
-                    <A href="/register" class="font-[thin-font] font-bold border border-dark-green transition ease-in delay-20 px-8 py-2 rounded-[16px]">გაიგე მეტი</A>
+                    <A href="/register" class="font-[thin-font] font-bold text-center bg-dark-green hover:bg-dark-green-hover transition ease-in delay-20 text-white px-8 py-2 rounded-[16px]">შექმენი პოსტი</A>
+                    <A href="/register" class="font-[thin-font] font-bold text-center border border-dark-green transition ease-in delay-20 px-8 py-2 rounded-[16px]">გაიგე მეტი</A>
                 </div>
             </div>
+            <img src={job_hunt} alt="Job complete" />
         </div>
         <div class="flex justify-center gap-x-12 border-l-4 border-slate-300 pl-8">
             <img src={interview} alt="Job complete" />
@@ -76,13 +77,12 @@ export const ThirdHero = () => {
                     <li>მიღება და შეფასება</li>
                 </ul>
                 <div class="flex mt-8 gap-y-2 flex-col">
-                    <A href="/register" class="font-[thin-font] font-bold bg-dark-green hover:bg-dark-green-hover transition ease-in delay-20 text-white px-8 py-2 rounded-[16px]">შექმენი პოსტი</A>
-                    <A href="/register" class="font-[thin-font] font-bold border border-dark-green transition ease-in delay-20 px-8 py-2 rounded-[16px]">გაიგე მეტი</A>
+                    <A href="/register" class="font-[thin-font] font-bold text-center bg-dark-green hover:bg-dark-green-hover transition ease-in delay-20 text-white px-8 py-2 rounded-[16px]">შექმენი პოსტი</A>
+                    <A href="/register" class="font-[thin-font] font-bold text-center border border-dark-green transition ease-in delay-20 px-8 py-2 rounded-[16px]">გაიგე მეტი</A>
                 </div>
             </div>
         </div>
         <div class="flex justify-center gap-x-12 border-l-4 border-slate-300 pl-8">
-            <img src={payment} alt="Job complete" />
             <div>
                 <h2 class="font-[font-bolder] text-xl font-bold">დასრულება</h2>
                 <ul class="font-[font-normal] text-lg text-gray mt-3 list-disc ml-5">
@@ -90,10 +90,11 @@ export const ThirdHero = () => {
                     <li>მიღება და შეფასება</li>
                 </ul>
                 <div class="flex mt-8 gap-y-2 flex-col">
-                    <A href="/register" class="font-[thin-font] font-bold bg-dark-green hover:bg-dark-green-hover transition ease-in delay-20 text-white px-8 py-2 rounded-[16px]">შექმენი პოსტი</A>
-                    <A href="/register" class="font-[thin-font] font-bold border border-dark-green transition ease-in delay-20 px-8 py-2 rounded-[16px]">გაიგე მეტი</A>
+                    <A href="/register" class="font-[thin-font] font-bold text-center bg-dark-green hover:bg-dark-green-hover transition ease-in delay-20 text-white px-8 py-2 rounded-[16px]">შექმენი პოსტი</A>
+                    <A href="/register" class="font-[thin-font] font-bold text-center border border-dark-green transition ease-in delay-20 px-8 py-2 rounded-[16px]">გაიგე მეტი</A>
                 </div>
             </div>
+            <img src={payment} alt="Job complete" />
         </div>
         <div class="flex justify-center gap-x-12 border-l-4 border-slate-300 pl-8">
             <img src={jobDone} alt="Job complete" />
@@ -104,8 +105,8 @@ export const ThirdHero = () => {
                     <li>მიღება და შეფასება</li>
                 </ul>
                 <div class="flex mt-8 gap-y-2 flex-col">
-                    <A href="/register" class="font-[thin-font] font-bold bg-dark-green hover:bg-dark-green-hover transition ease-in delay-20 text-white px-8 py-2 rounded-[16px]">შექმენი პოსტი</A>
-                    <A href="/register" class="font-[thin-font] font-bold border border-dark-green transition ease-in delay-20 px-8 py-2 rounded-[16px]">გაიგე მეტი</A>
+                    <A href="/register" class="font-[thin-font] font-bold text-center bg-dark-green hover:bg-dark-green-hover transition ease-in delay-20 text-white px-8 py-2 rounded-[16px]">შექმენი პოსტი</A>
+                    <A href="/register" class="font-[thin-font] font-bold text-center border border-dark-green transition ease-in delay-20 px-8 py-2 rounded-[16px]">გაიგე მეტი</A>
                 </div>
             </div>
         </div>
